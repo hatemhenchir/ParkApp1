b2d0ab7a3a0fc55353a0ec3c4234bcd3ac7ca9f3
malek-bt <malekbentaher3@gmail.com>
2022-05-15 13:26:35 +0100

    supp

--- android/app/src/main/kotlin/com/example/flutter_application_2/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.flutter_application_2
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
--- android/app/src/main/kotlin/com/example/parkapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.parkapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

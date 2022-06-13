193174b0552594034476412a17f55aca14f28416
malek-bt <malekbentaher3@gmail.com>
2022-06-13 20:30:09 +0100

    final

---
+++ build/stripe_android/generated/source/buildConfig/debug/com/flutter/stripe/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.flutter.stripe;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.flutter.stripe";
+  public static final String BUILD_TYPE = "debug";
+}
